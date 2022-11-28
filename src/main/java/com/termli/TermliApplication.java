13768d361f39c4dde554c8c6a7e3b869aec3b750
marcin <marcin7848@gmail.com>
2022-11-28 15:24:02 +0100

    project initialization

---
+++ src/main/java/com/termli/TermliApplication.java
@@ -0,0 +1,13 @@
+package com.termli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TermliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TermliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/termli/TermliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.termli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TermliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
